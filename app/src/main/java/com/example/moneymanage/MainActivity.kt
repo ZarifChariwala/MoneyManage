953eff332332018c4886f615f6f306a18473dce9
Zarif <67282878+ZarifChariwala@users.noreply.github.com>
2022-07-07 12:43:20 -0400

    Single Line = True

--- app/src/main/java/com/example/moneymanage/MainActivity.kt
+++ app/src/main/java/com/example/moneymanage/MainActivity.kt
@@ -1,11 +1,27 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.moneymanage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //hide status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        // Splash Screen dealying the screen for 3 second and coming to the home page
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        // 3000 is 3 second.
+        },3000)
     }
 }
